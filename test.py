8dab4ac981f4fbc4e93d374c187e82461d74b33e
harshareddy5209 <harshareddy5209@gmail.com>
2023-02-15 13:34:45 +0530

    firstcommit

---
+++ test.py
@@ -0,0 +1,6 @@
+#Program
+#test.py C:\Users\MAXHUB\my_repo1> folder
+
+print("Hello Students!!");     
+print("Welcome to Django");     
+print("ALL THE BEST");                                                
